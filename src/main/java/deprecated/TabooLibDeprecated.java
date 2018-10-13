175641c3562ad850d51fa220011b4842aab76e31
坏黑 <sky@asgard.me>
2018-10-13 17:41:25 +0800

    + to maven

---
+++ src/main/java/deprecated/TabooLibDeprecated.java
@@ -0,0 +1,11 @@
+package deprecated;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @Author 坏黑
+ * @Since 2018-10-13 16:55
+ */
+public class TabooLibDeprecated extends JavaPlugin {
+
+}
---
+++ src/main/java/obfuscated/RS.java
@@ -0,0 +1,46 @@
+package obfuscated;
+
+import java.util.Random;
+
+@Deprecated
+public class RS {
+
+    private static final Random RANDOM = new Random();
+    private static final char[] charsDown = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".toCharArray();
+    private static final char[] charsUp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".toCharArray();
+
+    public static char getRandomCharacter() {
+        return charsDown[RANDOM.nextInt(charsDown.length)];
+    }
+
+    public static char getRandomCharacter(Boolean isUp) {
+        return isUp ? charsUp[RANDOM.nextInt(charsUp.length)] : charsDown[RANDOM.nextInt(charsDown.length)];
+    }
+
+    public static String getRandomString(int length) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            builder.append(getRandomCharacter());
+        }
+        return builder.toString();
+    }
+
+    public static String getRandomString(String prefix, String rule, Boolean isUp) {
+        StringBuilder builder = new StringBuilder(prefix + "-");
+        String[] value = rule.split("-");
+
+        int size = 0;
+        for (String subrule : value) {
+            size++;
+
+            for (int i = 0; i < Integer.valueOf(subrule); i++) {
+                builder.append(getRandomCharacter(isUp));
+            }
+            if (!(size == value.length)) {
+                builder.append("-");
+            }
+        }
+
+        return builder.toString();
+    }
+}
