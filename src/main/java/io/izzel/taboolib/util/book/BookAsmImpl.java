4852fcc57f31840ccbe6430cfb4d31dbb07912a2
sky <sky@asgard.me>
2021-01-28 01:15:30 +0800

    1.0.3

--- src/main/java/io/izzel/taboolib/util/book/BookAsmImpl.java
+++ src/main/java/io/izzel/taboolib/util/book/BookAsmImpl.java
@@ -1,12 +1,16 @@
 package io.izzel.taboolib.util.book;
 
 import io.izzel.taboolib.Version;
+import io.izzel.taboolib.kotlin.Reflex;
 import io.izzel.taboolib.util.book.BookAsm;
 import io.izzel.taboolib.util.chat.BaseComponent;
 import io.izzel.taboolib.util.chat.ComponentSerializer;
 import net.minecraft.server.v1_16_R1.IChatBaseComponent;
 import org.bukkit.craftbukkit.v1_16_R1.inventory.CraftMetaBook;
+import org.bukkit.craftbukkit.v1_16_R3.util.CraftChatMessage;
 import org.bukkit.inventory.meta.BookMeta;
+
+import java.util.List;
 
 /**
  * @Author sky
@@ -26,7 +30,14 @@ public class BookAsmImpl extends BookAsm {
     public void addPages(BookMeta bookmeta, BaseComponent[]... pages) {
         for (BaseComponent[] components : pages) {
             if (v11600) {
-                ((org.bukkit.craftbukkit.v1_16_R1.inventory.CraftMetaBook) bookmeta).pages.add(net.minecraft.server.v1_16_R1.IChatBaseComponent.ChatSerializer.a(ComponentSerializer.toString(components)));
+                try {
+                    ((org.bukkit.craftbukkit.v1_16_R1.inventory.CraftMetaBook) bookmeta).pages.add(net.minecraft.server.v1_16_R1.IChatBaseComponent.ChatSerializer.a(ComponentSerializer.toString(components)));
+                } catch (Exception ignored) {
+                    List<String> p = Reflex.Companion.of(bookmeta).read("pages");
+                    if (p != null) {
+                        p.add(ComponentSerializer.toString(components));
+                    }
+                }
             } else {
                 ((org.bukkit.craftbukkit.v1_12_R1.inventory.CraftMetaBook) bookmeta).pages.add(net.minecraft.server.v1_12_R1.IChatBaseComponent.ChatSerializer.a(ComponentSerializer.toString(components)));
             }
